d320dc0e9caafbb349737e75ca859d85609dd72b
Az-zar-rt <beloshapka2001.09.03@gmail.com>
2022-08-07 00:54:11 +0300

    first commit

---
+++ Lesson2/Program.cs
@@ -0,0 +1,7 @@
+﻿double num = 5;
+double x = -6 * Math.Pow(num, 3) + 5 * Math.Pow(num, 2) - 10 * num + 15;
+Console.WriteLine(x);
+int num2 = 10;
+Console.WriteLine (Math.Abs(num2) * Math.Sin(num2));
+Console.WriteLine(Math.PI * num2 * 2);
+Console.WriteLine(Math.Max(num, num2));
